302c056fad91b7cae8b1de895dee0dd568924c41
Mouhamed NDOYE <49942742+ndoyeahmed@users.noreply.github.com>
2024-06-09 16:38:17 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // url: 'http://localhost:8091'
   // url: 'https://grif-api.grifsn.online/seminaireapi'
-  url: 'https://hackathon-api-972f4864806a.herokuapp.com'
+  url: 'https://hackaton-api-972f4864806a.herokuapp.com'
 };
